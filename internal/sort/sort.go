f83ff881aa83616192b6313fc890b9bd9e86dde4
Camilla H. Jensen <camije@ihelse.net>
2024-12-03 08:46:32 +0100

    Feat: Add function for sorting before merging

--- internal/sort/sort.go
+++ internal/sort/sort.go
@@ -7,11 +7,29 @@ import (
 	"github.com/hbesfb/bedfusion/internal/bed"
 )
 
-// Lexicographic sorting, based on chromosome, start and stop positions
+// Lexicographic sorting
+// Sorting order: chr, start, stop, strand, feat
 func lexicographicSort(lines []bed.Line) []bed.Line {
 	slices.SortFunc(lines, func(a, b bed.Line) int {
 		return cmp.Or(
 			cmp.Compare(a.Chr, b.Chr),
+			cmp.Compare(a.Start, b.Start),
+			cmp.Compare(a.Stop, b.Stop),
+			cmp.Compare(a.Strand, b.Strand),
+			cmp.Compare(a.Feat, b.Feat),
+		)
+	})
+	return lines
+}
+
+// Sorting used before merging
+// Sorting order: feat, chr, strand, start, stop
+func mergeSort(lines []bed.Line) []bed.Line {
+	slices.SortFunc(lines, func(a, b bed.Line) int {
+		return cmp.Or(
+			cmp.Compare(a.Feat, b.Feat),
+			cmp.Compare(a.Chr, b.Chr),
+			cmp.Compare(a.Strand, b.Strand),
 			cmp.Compare(a.Start, b.Start),
 			cmp.Compare(a.Stop, b.Stop),
 		)
--- internal/sort/sort_test.go
+++ internal/sort/sort_test.go
@@ -1,7 +1,6 @@
 package sort
 
 import (
-	"fmt"
 	"os"
 	"testing"
 
@@ -82,13 +81,13 @@ func TestLexicographicSort(t *testing.T) {
 				},
 				{
 					Chr: "1", Start: 10, Stop: 11,
-					Strand: "1", Feat: "A",
-					Full: []string{"1", "10", "11", "1", "A"},
+					Strand: "-1", Feat: "B",
+					Full: []string{"1", "10", "11", "-1", "B"},
 				},
 				{
 					Chr: "1", Start: 10, Stop: 11,
-					Strand: "-1", Feat: "B",
-					Full: []string{"1", "10", "11", "-1", "B"},
+					Strand: "1", Feat: "A",
+					Full: []string{"1", "10", "11", "1", "A"},
 				},
 				{
 					Chr: "1", Start: 12, Stop: 13,
@@ -118,7 +117,73 @@ func TestLexicographicSort(t *testing.T) {
 		t.Run(tc.testing, func(t *testing.T) {
 			t.Parallel()
 			receivedLines := lexicographicSort(tc.lines)
-			fmt.Printf("%+v", receivedLines)
+			if diff := deep.Equal(tc.expectedLines, receivedLines); diff != nil {
+				t.Error("expected VS received lines", diff)
+			}
+		})
+	}
+}
+
+func TestMergeSort(t *testing.T) {
+	t.Parallel()
+	type testCase struct {
+		testing       string
+		lines         []bed.Line
+		expectedLines []bed.Line
+	}
+	testCases := []testCase{
+		{
+			testing: "linesToSort",
+			lines:   linesToSort,
+			expectedLines: []bed.Line{
+				{
+					Chr: "1", Start: 10, Stop: 11,
+					Strand: "-1", Feat: "A",
+					Full: []string{"1", "10", "11", "-1", "A"},
+				},
+				{
+					Chr: "1", Start: 10, Stop: 11,
+					Strand: "1", Feat: "A",
+					Full: []string{"1", "10", "11", "1", "A"},
+				},
+				{
+					Chr: "1", Start: 12, Stop: 13,
+					Strand: "1", Feat: "A",
+					Full: []string{"1", "12", "13", "1", "A"},
+				},
+				{
+					Chr: "X", Start: 10, Stop: 11,
+					Strand: "1", Feat: "A",
+					Full: []string{"X", "10", "11", "1", "A"},
+				},
+				{
+					Chr: "1", Start: 8, Stop: 9,
+					Strand: "-1", Feat: "B",
+					Full: []string{"1", "8", "9", "-1", "B"},
+				},
+				{
+					Chr: "1", Start: 10, Stop: 11,
+					Strand: "-1", Feat: "B",
+					Full: []string{"1", "10", "11", "-1", "B"},
+				},
+				{
+					Chr: "2", Start: 12, Stop: 13,
+					Strand: "1", Feat: "C",
+					Full: []string{"2", "12", "13", "1", "C"},
+				},
+				{
+					Chr: "10", Start: 12, Stop: 13,
+					Strand: "1", Feat: "D",
+					Full: []string{"10", "12", "13", "1", "D"},
+				},
+			},
+		},
+	}
+	for _, tc := range testCases {
+		tc := tc
+		t.Run(tc.testing, func(t *testing.T) {
+			t.Parallel()
+			receivedLines := mergeSort(tc.lines)
 			if diff := deep.Equal(tc.expectedLines, receivedLines); diff != nil {
 				t.Error("expected VS received lines", diff)
 			}
