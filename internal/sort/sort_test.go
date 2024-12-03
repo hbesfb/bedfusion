ef2198057ad038b7b838a1853d799a31e90d6fa6
Camilla H. Jensen <camije@ihelse.net>
2024-12-03 08:30:10 +0100

    Test: Update test cases to make them easier to read

--- internal/sort/sort_test.go
+++ internal/sort/sort_test.go
@@ -15,6 +15,49 @@ func TestMain(m *testing.M) {
 	os.Exit(m.Run())
 }
 
+var linesToSort = []bed.Line{
+	{
+		Chr: "2", Start: 12, Stop: 13,
+		Strand: "1", Feat: "C",
+		Full: []string{"2", "12", "13", "1", "C"},
+	},
+	{
+		Chr: "1", Start: 8, Stop: 9,
+		Strand: "-1", Feat: "B",
+		Full: []string{"1", "8", "9", "-1", "B"},
+	},
+	{
+		Chr: "10", Start: 12, Stop: 13,
+		Strand: "1", Feat: "D",
+		Full: []string{"10", "12", "13", "1", "D"},
+	},
+	{
+		Chr: "X", Start: 10, Stop: 11,
+		Strand: "1", Feat: "A",
+		Full: []string{"X", "10", "11", "1", "A"},
+	},
+	{
+		Chr: "1", Start: 10, Stop: 11,
+		Strand: "-1", Feat: "A",
+		Full: []string{"1", "10", "11", "-1", "A"},
+	},
+	{
+		Chr: "1", Start: 12, Stop: 13,
+		Strand: "1", Feat: "A",
+		Full: []string{"1", "12", "13", "1", "A"},
+	},
+	{
+		Chr: "1", Start: 10, Stop: 11,
+		Strand: "1", Feat: "A",
+		Full: []string{"1", "10", "11", "1", "A"},
+	},
+	{
+		Chr: "1", Start: 10, Stop: 11,
+		Strand: "-1", Feat: "B",
+		Full: []string{"1", "10", "11", "-1", "B"},
+	},
+}
+
 func TestLexicographicSort(t *testing.T) {
 	t.Parallel()
 	type testCase struct {
@@ -24,89 +67,48 @@ func TestLexicographicSort(t *testing.T) {
 	}
 	testCases := []testCase{
 		{
-			testing: "complex bed",
-			lines: []bed.Line{
-				{
-					Chr: "2", Start: 31747549, Stop: 31763836,
-					Strand: "-1", Feat: "ENSG00000049319",
-					Full: []string{"2", "31747549", "31763836", "-1", "SRD5A2", "ENSG00000049319"},
-				},
-				{
-					Chr: "10", Start: 126085871, Stop: 126107545,
-					Strand: "-1", Feat: "ENSG00000065154",
-					Full: []string{"10", "126085871", "126107545", "-1", "OAT", "ENSG00000065154"},
-				},
-				{
-					Chr: "10", Start: 124768494, Stop: 124773587,
-					Strand: "1", Feat: "ENSG00000196177",
-					Full: []string{"10", "124768494", "124773587", "1", "ACADSB", "ENSG00000196177"},
-				},
-				{
-					Chr: "1", Start: 948802, Stop: 949920,
-					Strand: "1", Feat: "ENSG00000187608",
-					Full: []string{"1", "948802", "949920", "1", "ISG15", "ENSG00000187608"},
-				},
-				{
-					Chr: "X", Start: 135067597, Stop: 135129423,
-					Strand: "1", Feat: "ENSG00000198689",
-					Full: []string{"X", "135067597", "135129423", "1", "SLC9A6", "ENSG00000198689"},
-				},
-				{
-					Chr: "2", Start: 1800689, Stop: 31806136,
-					Strand: "-1", Feat: "ENSG00000049319",
-					Full: []string{"2", "31800689", "31806136", "-1", "SRD5A2", "ENSG00000049319"},
-				},
-				{
-					Chr: "10", Start: 124782049, Stop: 124817827,
-					Strand: "1", Feat: "ENSG00000196177",
-					Full: []string{"10", "124782049", "124817827", "1", "ACADSB", "ENSG00000196177"},
-				},
-				{
-					Chr: "1", Start: 860259, Stop: 879955,
-					Strand: "1", Feat: "ENSG00000187634",
-					Full: []string{"1", "860259", "879955", "1", "SAMD11", "ENSG00000187634"},
-				},
-			},
+			testing: "linesToSort",
+			lines:   linesToSort,
 			expectedLines: []bed.Line{
 				{
-					Chr: "1", Start: 860259, Stop: 879955,
-					Strand: "1", Feat: "ENSG00000187634",
-					Full: []string{"1", "860259", "879955", "1", "SAMD11", "ENSG00000187634"},
+					Chr: "1", Start: 8, Stop: 9,
+					Strand: "-1", Feat: "B",
+					Full: []string{"1", "8", "9", "-1", "B"},
 				},
 				{
-					Chr: "1", Start: 948802, Stop: 949920,
-					Strand: "1", Feat: "ENSG00000187608",
-					Full: []string{"1", "948802", "949920", "1", "ISG15", "ENSG00000187608"},
+					Chr: "1", Start: 10, Stop: 11,
+					Strand: "-1", Feat: "A",
+					Full: []string{"1", "10", "11", "-1", "A"},
 				},
 				{
-					Chr: "10", Start: 124768494, Stop: 124773587,
-					Strand: "1", Feat: "ENSG00000196177",
-					Full: []string{"10", "124768494", "124773587", "1", "ACADSB", "ENSG00000196177"},
+					Chr: "1", Start: 10, Stop: 11,
+					Strand: "1", Feat: "A",
+					Full: []string{"1", "10", "11", "1", "A"},
 				},
 				{
-					Chr: "10", Start: 124782049, Stop: 124817827,
-					Strand: "1", Feat: "ENSG00000196177",
-					Full: []string{"10", "124782049", "124817827", "1", "ACADSB", "ENSG00000196177"},
+					Chr: "1", Start: 10, Stop: 11,
+					Strand: "-1", Feat: "B",
+					Full: []string{"1", "10", "11", "-1", "B"},
 				},
 				{
-					Chr: "10", Start: 126085871, Stop: 126107545,
-					Strand: "-1", Feat: "ENSG00000065154",
-					Full: []string{"10", "126085871", "126107545", "-1", "OAT", "ENSG00000065154"},
+					Chr: "1", Start: 12, Stop: 13,
+					Strand: "1", Feat: "A",
+					Full: []string{"1", "12", "13", "1", "A"},
 				},
 				{
-					Chr: "2", Start: 1800689, Stop: 31806136,
-					Strand: "-1", Feat: "ENSG00000049319",
-					Full: []string{"2", "31800689", "31806136", "-1", "SRD5A2", "ENSG00000049319"},
+					Chr: "10", Start: 12, Stop: 13,
+					Strand: "1", Feat: "D",
+					Full: []string{"10", "12", "13", "1", "D"},
 				},
 				{
-					Chr: "2", Start: 31747549, Stop: 31763836,
-					Strand: "-1", Feat: "ENSG00000049319",
-					Full: []string{"2", "31747549", "31763836", "-1", "SRD5A2", "ENSG00000049319"},
+					Chr: "2", Start: 12, Stop: 13,
+					Strand: "1", Feat: "C",
+					Full: []string{"2", "12", "13", "1", "C"},
 				},
 				{
-					Chr: "X", Start: 135067597, Stop: 135129423,
-					Strand: "1", Feat: "ENSG00000198689",
-					Full: []string{"X", "135067597", "135129423", "1", "SLC9A6", "ENSG00000198689"},
+					Chr: "X", Start: 10, Stop: 11,
+					Strand: "1", Feat: "A",
+					Full: []string{"X", "10", "11", "1", "A"},
 				},
 			},
 		},
